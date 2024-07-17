83f2e7637e3c15847608081d941e56d28c126e4a
AndreaIngargiola <92037242+AndreaIngargiola@users.noreply.github.com>
2024-07-17 14:06:36 +0200

    feat: add CameraManager actor and supporting classes

---
+++ application/src/main/scala/utils/ProcessManager.scala
@@ -0,0 +1,28 @@
+package utils
+
+import scala.language.postfixOps
+import scala.util.{Failure, Success, Try}
+import sys.process.*
+
+sealed trait ProcessManager(instruction: String):
+
+  def execute(): String =
+    val command = "cmd /c "+ instruction
+    val result = Try:
+      val output = Process(command).!!
+      output
+
+    result match
+      case Success(output) =>
+        "Command executed successfully: " +output
+      case Failure(exception) =>
+        "Error executing command:" + {exception.getMessage}
+
+class TestProcessManager extends ProcessManager("echo test99")
+
+class CVLauncher(val args:List[String]) extends ProcessManager("../domain/src/build/app "+ args.toArray.mkString("Array(", ", ", ")"))
+
+class ExitApp extends ProcessManager("/'^C'")
+
+
+
