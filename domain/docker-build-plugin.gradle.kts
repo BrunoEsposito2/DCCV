919566f653f32202c5b5d9680023383677b65f35
BrunoEsposito2 <brunoesposito96@gmail.com>
2024-07-25 14:23:01 +0200

    build: fix gradle test task

--- domain/docker-build-plugin.gradle.kts
+++ domain/docker-build-plugin.gradle.kts
@@ -31,9 +31,9 @@ tasks.register("dockerBuild") {
             exec {
                 commandLine(
                     "sh", "-c", """
-                        echo "${project.projectDir.parent}" &&
+                        echo "${project.projectDir}" &&
                         echo "pwd stampato" &&
-                        docker run -v "${project.projectDir.parent}":/workspace -v /workspace/.gradle -v "${project.projectDir.parent}"/.gradle:/tmp/.gradle --name ubuntu-opencv_build-container --rm ubuntu-opencv_build /bin/bash -c 'GRADLE_USER_HOME=/tmp/.gradle ./gradlew build'
+                        docker run -v "${project.projectDir}":/workspace -v /workspace/.gradle -v "${project.projectDir}"/.gradle:/tmp/.gradle --name ubuntu-opencv_build-container --rm ubuntu-opencv_build /bin/bash -c 'GRADLE_USER_HOME=/tmp/.gradle ./gradlew build'
                     """)
                 standardOutput = System.out
                 errorOutput = System.err
