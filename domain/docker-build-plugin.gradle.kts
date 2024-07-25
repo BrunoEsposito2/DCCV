5d407372c7026109cb12ac7738ecb63383d179e4
BrunoEsposito2 <brunoesposito96@gmail.com>
2024-07-26 00:49:57 +0200

    build: modify gradle test task

--- domain/docker-build-plugin.gradle.kts
+++ domain/docker-build-plugin.gradle.kts
@@ -19,30 +19,30 @@ tasks.register("dockerBuild") {
                 errorOutput = System.err
             }
         }
-    } else if (OperatingSystem.current().isLinux) {
-        doFirst {
-            exec {
-                commandLine("sh", "-c", "docker build -t ubuntu-opencv_build .")
-                standardOutput = System.out
-                errorOutput = System.err
-            }
-        }
-        doLast {
-            val mounts = project.projectDir.parentFile.listFiles()
-                ?.filter { x -> !(x.name == ".gradle" || x.name == ".idea") }
-                ?.map { "-v ${it.absolutePath}:/workspace/${it.name}" }
-                ?.map { it.replace(",", " ") }
-                ?.joinToString(" ")
-            println("MOUNTS: \n" + mounts)
-            exec {
-                commandLine(
-                    "sh", "-c", "docker run ${mounts} --name ubuntu-opencv_build-container --rm ubuntu-opencv_build /bin/bash -c './gradlew build'"
-                )
-                standardOutput = System.out
-                errorOutput = System.err
-            }
-        }
-    } else if (System.getenv("GITHUB_ACTIONS") == "true") {
+//    } else if (OperatingSystem.current().isLinux) {
+//        doFirst {
+//            exec {
+//                commandLine("sh", "-c", "docker build -t ubuntu-opencv_build .")
+//                standardOutput = System.out
+//                errorOutput = System.err
+//            }
+//        }
+//        doLast {
+//            val mounts = project.projectDir.parentFile.listFiles()
+//                ?.filter { x -> !(x.name == ".gradle" || x.name == ".idea") }
+//                ?.map { "-v ${it.absolutePath}:/workspace/${it.name}" }
+//                ?.map { it.replace(",", " ") }
+//                ?.joinToString(" ")
+//            println("MOUNTS: \n" + mounts)
+//            exec {
+//                commandLine(
+//                    "sh", "-c", "docker run ${mounts} --name ubuntu-opencv_build-container --rm ubuntu-opencv_build /bin/bash -c './gradlew build'"
+//                )
+//                standardOutput = System.out
+//                errorOutput = System.err
+//            }
+//        }
+    } else if (OperatingSystem.current().isLinux) { //(System.getenv("GITHUB_ACTIONS") == "true") {
         exec {
             commandLine("sh", "-c", "./gradlew build")
         }
