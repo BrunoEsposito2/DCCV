11f7ea50b4f6dc4bd1a8524e9850b9208a5fa12a
BrunoEsposito2 <brunoesposito96@gmail.com>
2024-11-18 21:37:51 +0000

    fix: change video format

--- domain/run.sh
+++ domain/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( pwd )"
 export LD_LIBRARY_PATH="$DIR/build/release/domain/libs:$LD_LIBRARY_PATH"
-"$DIR/build/release/domain/bin/domain" -v="$DIR/video/output.avi"
+"$DIR/build/release/domain/bin/domain" -v="$DIR/video/video.avi"
