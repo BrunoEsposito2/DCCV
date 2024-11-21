8126616f53a3b5a1f2359933dcb8793f814e077b
BrunoEsposito2 <brunoesposito96@gmail.com>
2024-11-21 21:06:42 +0000

    chore: add stream video routing and sending related data

--- domain/run.sh
+++ domain/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export LD_LIBRARY_PATH="$DIR/libs:$LD_LIBRARY_PATH"
-"$DIR/bin/domain" -v="/workspace/domain/video/video.avi"
+"$DIR/bin/domain" -v="/workspace/domain/video/video.avi" -id=3
