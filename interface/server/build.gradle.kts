d3abf45fb766cb688c47ccdd10f83d79e35c0a92
AndreaIngargiola <92037242+AndreaIngargiola@users.noreply.github.com>
2025-05-15 12:40:16 +0200

    fix: change server build.gradle

--- interface/server/build.gradle.kts
+++ interface/server/build.gradle.kts
@@ -67,11 +67,11 @@ tasks {
 // Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
 }
 
 application {
     // Define the main class for the application.
-    mainClass = "actor.server"
+    mainClass.set("launcher")
 }
